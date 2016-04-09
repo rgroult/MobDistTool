a78c25d470998df223ac55eb53fbf2e22ff62f72
gogetta <gogetta@gogettas-macbook.home>
2016-04-09 14:04:44 +0200

    add precision in file's copyright

--- server/config/src/storage.dart
+++ server/config/src/storage.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, the Dart project authors.
+// Copyright (c) 2016, the Mobile App Distribution Tool project authors.
 // All rights reserved. Use of this source code is governed by a
 // MIT-style license that can be found in the LICENSE file.
 
