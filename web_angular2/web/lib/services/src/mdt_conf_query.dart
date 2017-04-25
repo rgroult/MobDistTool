a65d62f16bea2cfcd7989184f634c47969c84393
rgroult <gogetta@gogettasmacbook.home>
2017-04-25 22:06:15 +0200

    fix config

--- web_angular2/web/lib/services/src/mdt_conf_query.dart
+++ web_angular2/web/lib/services/src/mdt_conf_query.dart
@@ -1,10 +1,9 @@
 import 'dart:core';
 
-//String mdtServerApiRootUrl = "/api" ;// const String.fromEnvironment('mode') == 'javascript' ? "/api" : "http://localhost:8080/api";
-String mdtServerApiRootUrl = "http://localhost:8080/api";
+String mdtServerApiRootUrl = "/api" ;// const String.fromEnvironment('mode') == 'javascript' ? "/api" : "http://localhost:8080/api";
+//String mdtServerApiRootUrl = "https://localhost:8080/api";
 final String appVersion = "v1";
 final String appPath = "/applications/${appVersion}";
 final String artifactsPath = "/art/${appVersion}";
 final String inPath = "/in/${appVersion}";
 final String usersPath = "/users/${appVersion}";
-
